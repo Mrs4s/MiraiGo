236c0f629099171874537b2297bc6ede65c517a8
Mrs4s <mrs4sxiaoshi@gmail.com>
2020-09-10 09:39:44 +0800

    fix reply.

--- client/client.go
+++ client/client.go
@@ -296,6 +296,11 @@ func (c *QQClient) SendGroupMessage(groupCode int64, m *message.SendingMessage, 
 		useFram = f[0]
 	}
 	imgCount := m.Count(func(e message.IMessageElement) bool { return e.Type() == message.Image })
+	if useFram {
+		if m.Any(func(e message.IMessageElement) bool { return e.Type() == message.Reply }) {
+			useFram = false
+		}
+	}
 	msgLen := message.EstimateLength(m.Elements, 703)
 	if msgLen > 5000 || imgCount > 50 {
 		return nil
