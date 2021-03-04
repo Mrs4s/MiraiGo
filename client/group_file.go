3f91730dc4e97b9d1f60439764a1f360ccf49337
wdvxdr <wdvxdr@foxmail.com>
2021-03-04 13:26:37 +0800

    feat: improve upload file parallel 相同的文件不会并行上传

--- client/group_file.go
+++ client/group_file.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"os"
 	"runtime/debug"
+	"sync"
 
 	"google.golang.org/protobuf/proto"
 
@@ -53,6 +54,8 @@ type (
 		TotalFileCount uint32 `json:"total_file_count"`
 	}
 )
+
+var fileSingleFlight = sync.Map{}
 
 func init() {
 	decoders["OidbSvc.0x6d8_1"] = decodeOIDB6d81Response
@@ -156,6 +159,17 @@ func (fs *GroupFileSystem) GetFilesByFolder(folderId string) ([]*GroupFile, []*G
 }
 
 func (fs *GroupFileSystem) UploadFile(p, name, folderId string) error {
+	// 同文件等待其他线程上传
+	if wg, ok := fileSingleFlight.Load(p); ok {
+		wg.(*sync.WaitGroup).Wait()
+	} else {
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		fileSingleFlight.Store(p, wg)
+		defer wg.Done()
+		defer fileSingleFlight.Delete(p)
+	}
+
 	file, err := os.OpenFile(p, os.O_RDONLY, 0666)
 	if err != nil {
 		return errors.Wrap(err, "open file error")
