d87c3546498b9809477e4aeddf20ab0e15df38a2
Mrs4s <mrs4sxiaoshi@gmail.com>
2021-03-18 20:53:44 +0800

    add t16.go.

---
+++ protocol/tlv/t16.go
@@ -0,0 +1,18 @@
+package tlv
+
+import "github.com/Mrs4s/MiraiGo/binary"
+
+func T16(ssoVersion, appId, subAppId uint32, guid, apkId, apkVersionName, apkSign []byte) []byte {
+	return binary.NewWriterF(func(w *binary.Writer) {
+		w.WriteUInt16(0x16)
+		w.WriteTlv(binary.NewWriterF(func(w *binary.Writer) {
+			w.WriteUInt32(ssoVersion)
+			w.WriteUInt32(appId)
+			w.WriteUInt32(subAppId)
+			w.Write(guid)
+			w.WriteTlv(apkId)
+			w.WriteTlv(apkVersionName)
+			w.WriteTlv(apkSign)
+		}))
+	})
+}
