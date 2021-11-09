75d6d547d217ab957b98b11d4a025a39cab4ce6b
Mrs4s <mrs4sxiaoshi@gmail.com>
2021-11-10 04:12:01 +0800

    feat: SendGuildChannelMessage

--- client/guild_msg.go
+++ client/guild_msg.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"encoding/hex"
+	"math/rand"
 	"strconv"
 
 	"github.com/Mrs4s/MiraiGo/client/pb/cmd0x388"
@@ -9,36 +10,48 @@ import (
 
 	"github.com/pkg/errors"
 
-	"time"
-
 	"github.com/Mrs4s/MiraiGo/client/pb/channel"
 	"github.com/Mrs4s/MiraiGo/client/pb/msg"
 	"github.com/Mrs4s/MiraiGo/message"
 	"google.golang.org/protobuf/proto"
 )
 
-func init() {
-	decoders["MsgPush.PushGroupProMsg"] = decodeGuildMessagePushPacket
-}
-
-func (c *QQClient) parseGuildChannelMessage(msg *channel.ChannelMsgContent) *message.GuildChannelMessage {
-	guild := c.GuildService.FindGuild(msg.Head.RoutingHead.GetGuildId())
-	if guild == nil {
-		return nil // todo: sync guild info
-	}
-	// mem := guild.FindMember(msg.Head.RoutingHead.GetFromTinyid())
-	return &message.GuildChannelMessage{
-		Id:         msg.Head.ContentHead.GetSeq(),
-		InternalId: msg.Body.RichText.Attr.GetRandom(),
-		GuildId:    msg.Head.RoutingHead.GetGuildId(),
-		ChannelId:  msg.Head.RoutingHead.GetChannelId(),
-		Time:       int64(msg.Head.ContentHead.GetTime()),
-		Sender: &message.GuildSender{
-			TinyId:   msg.Head.RoutingHead.GetFromTinyid(),
-			Nickname: string(msg.ExtInfo.GetFromNick()),
+func (s *GuildService) SendGuildChannelMessage(guildId, channelId uint64, m *message.SendingMessage) error {
+	mr := rand.Uint32() // 客户端似乎是生成的 u32 虽然类型是u64
+	req := &channel.DF62ReqBody{Msg: &channel.ChannelMsgContent{
+		Head: &channel.ChannelMsgHead{
+			RoutingHead: &channel.ChannelRoutingHead{
+				GuildId:   &guildId,
+				ChannelId: &channelId,
+				FromUin:   proto.Uint64(uint64(s.c.Uin)),
+			},
+			ContentHead: &channel.ChannelContentHead{
+				Type:   proto.Uint64(3840), // const
+				Random: proto.Uint64(uint64(mr)),
+			},
 		},
-		Elements: message.ParseMessageElems(msg.Body.RichText.Elems),
+		Body: &msg.MessageBody{
+			RichText: &msg.RichText{
+				Elems: message.ToProtoElems(m.Elements, true),
+			},
+		},
+	}}
+	seq := s.c.nextSeq()
+	payload, _ := proto.Marshal(req)
+	packet := packets.BuildUniPacket(s.c.Uin, seq, "MsgProxy.SendMsg", 1, s.c.OutGoingPacketSessionId, []byte{}, s.c.sigInfo.d2Key, payload)
+	rsp, err := s.c.sendAndWaitDynamic(seq, packet)
+	if err != nil {
+		return errors.Wrap(err, "send packet error")
 	}
+	body := new(channel.DF62RspBody)
+	if err = proto.Unmarshal(rsp, body); err != nil {
+		return errors.Wrap(err, "failed to unmarshal protobuf message")
+	}
+	if body.GetResult() != 0 {
+		return errors.Errorf("send channel message error: server response %v", body.GetResult())
+	}
+	// todo: 返回 *message.GuildMessage
+	return nil
 }
 
 func (s *GuildService) QueryImage(guildId, channelId uint64, hash []byte, size uint64) (*message.GuildImageElement, error) {
@@ -91,94 +104,6 @@ func (s *GuildService) QueryImage(guildId, channelId uint64, hash []byte, size u
 	return nil, errors.New("image is not exists")
 }
 
-func decodeGuildMessagePushPacket(c *QQClient, _ *incomingPacketInfo, payload []byte) (interface{}, error) {
-	push := new(channel.MsgOnlinePush)
-	if err := proto.Unmarshal(payload, push); err != nil {
-		return nil, errors.Wrap(err, "failed to unmarshal protobuf message")
-	}
-	for _, m := range push.Msgs {
-		if m.Head.ContentHead.GetType() == 3841 {
-			type tipsPushInfo struct {
-				TinyId                 uint64
-				TargetMessageSenderUin int64
-				GuildId                uint64
-				ChannelId              uint64
-			}
-			// todo: 回头 event flow 的处理移出去重构下逻辑, 先暂时这样方便改
-			var common *msg.CommonElem
-			if m.Body != nil {
-				for _, e := range m.Body.RichText.Elems {
-					if e.CommonElem != nil {
-						common = e.CommonElem
-						break
-					}
-				}
-			}
-			if m.Head.ContentHead.GetSubType() == 2 { // todo: tips?
-				if common == nil { // empty tips
-
-				}
-				tipsInfo := &tipsPushInfo{
-					TinyId:    m.Head.RoutingHead.GetFromTinyid(),
-					GuildId:   m.Head.RoutingHead.GetGuildId(),
-					ChannelId: m.Head.RoutingHead.GetChannelId(),
-				}
-				if len(m.CtrlHead.IncludeUin) > 0 {
-					tipsInfo.TargetMessageSenderUin = int64(m.CtrlHead.IncludeUin[0])
-				}
-				return tipsInfo, nil
-			}
-			if common == nil || common.GetServiceType() != 500 {
-				continue
-			}
-			eventBody := new(channel.EventBody)
-			if err := proto.Unmarshal(common.PbElem, eventBody); err != nil {
-				c.Error("failed to unmarshal guild channel event body: %v", err)
-				continue
-			}
-			if eventBody.UpdateMsg != nil {
-				if eventBody.UpdateMsg.GetEventType() == 1 || eventBody.UpdateMsg.GetEventType() == 2 { // todo: 撤回消息
-					continue
-				}
-				if eventBody.UpdateMsg.GetEventType() == 4 { // 消息贴表情更新 (包含添加或删除)
-					t, err := c.GuildService.pullRoamMsgByEventFlow(m.Head.RoutingHead.GetGuildId(), m.Head.RoutingHead.GetChannelId(), eventBody.UpdateMsg.GetMsgSeq(), eventBody.UpdateMsg.GetMsgSeq(), eventBody.UpdateMsg.GetEventVersion()-1)
-					if err != nil || len(t) == 0 {
-						c.Error("process guild event flow error: pull eventMsg message error: %v", err)
-						continue
-					}
-					// 自己的消息被贴表情会单独推送一个tips, 这里不需要解析
-					if t[0].Head.RoutingHead.GetFromTinyid() == c.GuildService.TinyId {
-						continue
-					}
-					updatedEvent := &GuildMessageReactionsUpdatedEvent{
-						GuildId:          m.Head.RoutingHead.GetGuildId(),
-						ChannelId:        m.Head.RoutingHead.GetChannelId(),
-						MessageId:        t[0].Head.ContentHead.GetSeq(),
-						CurrentReactions: decodeGuildMessageEmojiReactions(t[0]),
-					}
-					tipsInfo, err := c.waitPacketTimeoutSyncF("MsgPush.PushGroupProMsg", time.Second, func(i interface{}) bool {
-						if i == nil {
-							return false
-						}
-						_, ok := i.(*tipsPushInfo)
-						return ok
-					})
-					if err == nil {
-						updatedEvent.OperatorId = tipsInfo.(*tipsPushInfo).TinyId
-						updatedEvent.MessageSenderUin = tipsInfo.(*tipsPushInfo).TargetMessageSenderUin
-					}
-					c.dispatchGuildMessageReactionsUpdatedEvent(updatedEvent)
-				}
-			}
-			continue
-		}
-		if cm := c.parseGuildChannelMessage(m); cm != nil {
-			c.dispatchGuildChannelMessage(cm)
-		}
-	}
-	return nil, nil
-}
-
 func decodeGuildMessageEmojiReactions(content *channel.ChannelMsgContent) (r []*message.GuildMessageEmojiReaction) {
 	r = []*message.GuildMessageEmojiReaction{}
 	var common *msg.CommonElem
@@ -213,4 +138,24 @@ func decodeGuildMessageEmojiReactions(content *channel.ChannelMsgContent) (r []*
 		}
 	}
 	return
+}
+
+func (c *QQClient) parseGuildChannelMessage(msg *channel.ChannelMsgContent) *message.GuildChannelMessage {
+	guild := c.GuildService.FindGuild(msg.Head.RoutingHead.GetGuildId())
+	if guild == nil {
+		return nil // todo: sync guild info
+	}
+	// mem := guild.FindMember(msg.Head.RoutingHead.GetFromTinyid())
+	return &message.GuildChannelMessage{
+		Id:         msg.Head.ContentHead.GetSeq(),
+		InternalId: msg.Body.RichText.Attr.GetRandom(),
+		GuildId:    msg.Head.RoutingHead.GetGuildId(),
+		ChannelId:  msg.Head.RoutingHead.GetChannelId(),
+		Time:       int64(msg.Head.ContentHead.GetTime()),
+		Sender: &message.GuildSender{
+			TinyId:   msg.Head.RoutingHead.GetFromTinyid(),
+			Nickname: string(msg.ExtInfo.GetFromNick()),
+		},
+		Elements: message.ParseMessageElems(msg.Body.RichText.Elems),
+	}
 }
