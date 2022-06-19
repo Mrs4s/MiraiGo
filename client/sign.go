c92096e7ae9361f5f963c0c78b9903300cebd4f4
synodriver <diguohuangjiajinweijun@gmail.com>
2022-06-19 15:18:41 +0800

    fix sign packet error (#278)
    * fix sign packet error
    
    * Update sign.go

--- client/sign.go
+++ client/sign.go
@@ -9,19 +9,18 @@ import (
 
 // SendGroupSign 发送群聊打卡消息
 func (c *QQClient) SendGroupSign(target int64) {
-	_, _ = c.sendAndWait(c.buildGroupSignPacket(target, 0))
+	_, _ = c.sendAndWait(c.buildGroupSignPacket(target))
 }
 
-func (c *QQClient) buildGroupSignPacket(groupId int64, scene uint32) (uint16, []byte) {
+func (c *QQClient) buildGroupSignPacket(groupId int64) (uint16, []byte) {
 	body := &oidb.DEB7ReqBody{
-		SignInStatusReq: &oidb.StSignInStatusReq{
+		SignInWriteReq: &oidb.StSignInWriteReq{
 			Uid:           proto.Some(strconv.Itoa(int(c.Uin))),
 			GroupId:       proto.Some(strconv.Itoa(int(groupId))),
-			Scene:         proto.Some(scene),
-			ClientVersion: proto.Some("8.5.0.5025"),
+			ClientVersion: proto.Some("8.5.0"),
 		},
 	}
 	b, _ := proto.Marshal(body)
-	payload := c.packOIDBPackage(3767, 0, b)
+	payload := c.packOIDBPackage(3767, 1, b)
 	return c.uniPacket("OidbSvc.0xeb7", payload)
 }
