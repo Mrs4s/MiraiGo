1e2ebc8e2a500893292768ac14a4ab99b2c2f530
icarus-ai <82353054+icarus-ai@users.noreply.github.com>
2022-08-22 13:35:59 +0800

    为 Statistics 实现 MarshalJSON 接口

--- client/statistics.go
+++ client/statistics.go
@@ -1,6 +1,13 @@
 package client
 
-import "sync/atomic"
+import (
+	"fmt"
+	"reflect"
+	"sync/atomic"
+
+	"github.com/Mrs4s/MiraiGo/binary"
+	"github.com/Mrs4s/MiraiGo/utils"
+)
 
 type Statistics struct {
 	PacketReceived  atomic.Uint64 `json:"packet_received"`
@@ -15,4 +22,30 @@ type Statistics struct {
 
 func (c *QQClient) GetStatistics() *Statistics {
 	return &c.stat
+}
+
+// MarshalJSON encodes the wrapped statistics into JSON.
+func (m *Statistics) MarshalJSON() ([]byte, error) {
+	value := reflect.ValueOf(m).Elem()
+	types := value.Type()
+
+	byt := binary.SelectWriter()
+	defer binary.PutWriter(byt)
+
+	byt.WriteByte('{')
+	for i:=0; i<value.NumField(); i++ {
+		if i != 0 { byt.WriteByte(',') }
+		tag := types.Field(i).Tag.Get("json")
+		v   := value.Field(i)
+		v    = v.Field(v.NumField()-1)
+		switch v.Kind() {
+			case reflect.Uint32, reflect.Uint64: byt.Write(utils.S2B(fmt.Sprintf(`"%s":%d`, tag, v.Uint())))
+			case reflect.Int64: byt.Write(utils.S2B(fmt.Sprintf(`"%s":%d`, tag, v.Int())))
+			// Statistics里暂时没有 atomic.Bool
+			// case reflect.Bool: byt.Write(utils.S2B(fmt.Sprintf(`"%s":%v`, tag, v.Bool())))
+		}
+	}
+	byt.WriteByte('}')
+
+	return byt.Bytes(), nil
 }
