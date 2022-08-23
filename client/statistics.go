410f261d0b101a735e7bc3d805133d38934c9c6a
icarus-ai <82353054+icarus-ai@users.noreply.github.com>
2022-08-23 19:34:47 +0800

    为 Statistics 实现 MarshalJSON 接口 (See 2d79fcc)

--- client/statistics.go
+++ client/statistics.go
@@ -1,23 +1,20 @@
 package client
 
 import (
-	"fmt"
-	"reflect"
+	"bytes"
+	"strconv"
 	"sync/atomic"
-
-	"github.com/Mrs4s/MiraiGo/binary"
-	"github.com/Mrs4s/MiraiGo/utils"
 )
 
 type Statistics struct {
-	PacketReceived  atomic.Uint64 `json:"packet_received"`
-	PacketSent      atomic.Uint64 `json:"packet_sent"`
-	PacketLost      atomic.Uint64 `json:"packet_lost"`
-	MessageReceived atomic.Uint64 `json:"message_received"`
-	MessageSent     atomic.Uint64 `json:"message_sent"`
-	LastMessageTime atomic.Int64  `json:"last_message_time"`
-	DisconnectTimes atomic.Uint32 `json:"disconnect_times"`
-	LostTimes       atomic.Uint32 `json:"lost_times"`
+	PacketReceived  atomic.Uint64
+	PacketSent      atomic.Uint64
+	PacketLost      atomic.Uint64
+	MessageReceived atomic.Uint64
+	MessageSent     atomic.Uint64
+	LastMessageTime atomic.Int64
+	DisconnectTimes atomic.Uint32
+	LostTimes       atomic.Uint32
 }
 
 func (c *QQClient) GetStatistics() *Statistics {
@@ -26,26 +23,24 @@ func (c *QQClient) GetStatistics() *Statistics {
 
 // MarshalJSON encodes the wrapped statistics into JSON.
 func (m *Statistics) MarshalJSON() ([]byte, error) {
-	value := reflect.ValueOf(m).Elem()
-	types := value.Type()
-
-	byt := binary.SelectWriter()
-	defer binary.PutWriter(byt)
-
-	byt.WriteByte('{')
-	for i:=0; i<value.NumField(); i++ {
-		if i != 0 { byt.WriteByte(',') }
-		tag := types.Field(i).Tag.Get("json")
-		v   := value.Field(i)
-		v    = v.Field(v.NumField()-1)
-		switch v.Kind() {
-			case reflect.Uint32, reflect.Uint64: byt.Write(utils.S2B(fmt.Sprintf(`"%s":%d`, tag, v.Uint())))
-			case reflect.Int64: byt.Write(utils.S2B(fmt.Sprintf(`"%s":%d`, tag, v.Int())))
-			// Statistics里暂时没有 atomic.Bool
-			// case reflect.Bool: byt.Write(utils.S2B(fmt.Sprintf(`"%s":%v`, tag, v.Bool())))
-		}
-	}
-	byt.WriteByte('}')
-
-	return byt.Bytes(), nil
+	var w bytes.Buffer
+	w.Grow(256)
+	w.WriteString(`{"packet_received":`)
+	w.WriteString(strconv.FormatUint(m.PacketReceived.Load(), 10))
+	w.WriteString(`,"packet_sent":`)
+	w.WriteString(strconv.FormatUint(m.PacketSent.Load(), 10))
+	w.WriteString(`,"packet_lost":`)
+	w.WriteString(strconv.FormatUint(m.PacketLost.Load(), 10))
+	w.WriteString(`,"message_received":`)
+	w.WriteString(strconv.FormatUint(m.MessageReceived.Load(), 10))
+	w.WriteString(`,"message_sent":`)
+	w.WriteString(strconv.FormatUint(m.MessageSent.Load(), 10))
+	w.WriteString(`,"disconnect_times":`)
+	w.WriteString(strconv.FormatUint(uint64(m.DisconnectTimes.Load()), 10))
+	w.WriteString(`,"lost_times":`)
+	w.WriteString(strconv.FormatUint(uint64(m.LostTimes.Load()), 10))
+	w.WriteString(`,"last_message_time":`)
+	w.WriteString(strconv.FormatInt(m.LastMessageTime.Load(), 10))
+	w.WriteByte('}')
+	return w.Bytes(), nil
 }
