08ca3b023a4d0d712bf60ce83c7a0cd2a9bb34e8
icarus-ai <82353054+icarus-ai@users.noreply.github.com>
2022-08-24 14:22:36 +0800

    deprecate CAS in favor of CompareAndSwap (#291)
    * deprecate CAS in favor of CompareAndSwap
    
    * ci: update to go1.19
    
    * 为 Statistics 实现 MarshalJSON 接口 (See 2d79fcc)

--- client/statistics.go
+++ client/statistics.go
@@ -1,20 +1,46 @@
 package client
 
 import (
-	"go.uber.org/atomic"
+	"bytes"
+	"strconv"
+	"sync/atomic"
 )
 
 type Statistics struct {
-	PacketReceived  atomic.Uint64 `json:"packet_received"`
-	PacketSent      atomic.Uint64 `json:"packet_sent"`
-	PacketLost      atomic.Uint64 `json:"packet_lost"`
-	MessageReceived atomic.Uint64 `json:"message_received"`
-	MessageSent     atomic.Uint64 `json:"message_sent"`
-	LastMessageTime atomic.Int64  `json:"last_message_time"`
-	DisconnectTimes atomic.Uint32 `json:"disconnect_times"`
-	LostTimes       atomic.Uint32 `json:"lost_times"`
+	PacketReceived  atomic.Uint64
+	PacketSent      atomic.Uint64
+	PacketLost      atomic.Uint64
+	MessageReceived atomic.Uint64
+	MessageSent     atomic.Uint64
+	LastMessageTime atomic.Int64
+	DisconnectTimes atomic.Uint32
+	LostTimes       atomic.Uint32
 }
 
 func (c *QQClient) GetStatistics() *Statistics {
 	return &c.stat
+}
+
+// MarshalJSON encodes the wrapped statistics into JSON.
+func (m *Statistics) MarshalJSON() ([]byte, error) {
+	var w bytes.Buffer
+	w.Grow(256)
+	w.WriteString(`{"packet_received":`)
+	w.WriteString(strconv.FormatUint(m.PacketReceived.Load(), 10))
+	w.WriteString(`,"packet_sent":`)
+	w.WriteString(strconv.FormatUint(m.PacketSent.Load(), 10))
+	w.WriteString(`,"packet_lost":`)
+	w.WriteString(strconv.FormatUint(m.PacketLost.Load(), 10))
+	w.WriteString(`,"message_received":`)
+	w.WriteString(strconv.FormatUint(m.MessageReceived.Load(), 10))
+	w.WriteString(`,"message_sent":`)
+	w.WriteString(strconv.FormatUint(m.MessageSent.Load(), 10))
+	w.WriteString(`,"disconnect_times":`)
+	w.WriteString(strconv.FormatUint(uint64(m.DisconnectTimes.Load()), 10))
+	w.WriteString(`,"lost_times":`)
+	w.WriteString(strconv.FormatUint(uint64(m.LostTimes.Load()), 10))
+	w.WriteString(`,"last_message_time":`)
+	w.WriteString(strconv.FormatInt(m.LastMessageTime.Load(), 10))
+	w.WriteByte('}')
+	return w.Bytes(), nil
 }
--- go.mod
+++ go.mod
@@ -1,23 +1,25 @@
 module github.com/Mrs4s/MiraiGo
 
-go 1.18
+go 1.19
 
 require (
-	github.com/RomiChan/protobuf v0.1.1-0.20220624030127-3310cba9dbc0
+	github.com/RomiChan/protobuf v0.0.0-20220624030127-3310cba9dbc0
 	github.com/RomiChan/syncx v0.0.0-20220404072119-d7ea0ae15a4c
 	github.com/fumiama/imgsz v0.0.2
-	github.com/pierrec/lz4/v4 v4.1.11
+	github.com/pierrec/lz4/v4 v4.1.15
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.7.0
-	github.com/tidwall/gjson v1.11.0
-	go.uber.org/atomic v1.9.0
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	github.com/stretchr/testify v1.8.0
+	github.com/tidwall/gjson v1.14.3
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
