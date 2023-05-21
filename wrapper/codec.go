425f3e4d0c7e31584174573195c8219d40b87bc4
dduu <1303384099@qq.com>
2023-05-21 18:50:36 +0800

    feat: pack sec head

--- wrapper/codec.go
+++ wrapper/codec.go
@@ -1,3 +1,7 @@
 package wrapper
 
 var DandelionEnergy func(uint64, string, string, []byte) ([]byte, error)
+
+var FekitGetSign func(uint64, string, string, string, []byte) ([]byte, []byte, []byte, error)
+
+var AllowSignSendMsg func() bool
