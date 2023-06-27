19e3d172596ea1b553bdb2b313ad1b44828106fc
Mrs4s <mrs4sxiaoshi@gmail.com>
2023-06-27 17:08:59 +0800

    update transport

--- wrapper/codec.go
+++ wrapper/codec.go
@@ -3,5 +3,3 @@ package wrapper
 var DandelionEnergy func(uint64, string, string, []byte) ([]byte, error)
 
 var FekitGetSign func(uint64, string, string, string, []byte) ([]byte, []byte, []byte, error)
-
-var AllowSignSendMsg func() bool
